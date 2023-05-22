666a658ac81cccd7f0251be32c58302f4c16efab
Roman Petrakov <development@telfia.com>
2023-05-22 11:40:37 +0300

    fix bugs after review

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -1,7 +1,7 @@
 from typing import List, Dict, Any
 
 from aiogoogle import Aiogoogle
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
@@ -19,7 +19,7 @@ router = APIRouter()
 
 @router.post(
     '/',
-    response_model=List[Dict[str, Any]],
+    response_model=str,
     dependencies=[Depends(current_superuser)],
 )
 async def get_report(
@@ -30,7 +30,14 @@ async def get_report(
     projects = await charity_project_crud.get_projects_by_completion_rate(
         session
     )
-    spreadsheet_id = await spreadsheets_create(wrapper_services)
+    spreadsheet_id, spreadsheet_url = await spreadsheets_create(
+        wrapper_services
+    )
     await set_user_permissions(spreadsheet_id, wrapper_services)
-    await spreadsheets_update_value(spreadsheet_id, projects, wrapper_services)
-    return projects
+    try:
+        await spreadsheets_update_value(
+            spreadsheet_id, projects, wrapper_services
+        )
+    except ValueError as error:
+        raise HTTPException(status_code=400, detail=str(error))
+    return spreadsheet_url
--- app/services/google_api.py
+++ app/services/google_api.py
@@ -1,55 +1,44 @@
+import copy
 from datetime import datetime
+from typing import Tuple
 
 from aiogoogle import Aiogoogle
 
 from app.core.config import settings
 
 FORMAT = "%Y/%m/%d %H:%M:%S"
-LOCALE = 'ru_RU'
 SPREADSHEET_TITLE = 'Отчет на {now_date_time}'
-SHEET_TITLE = 'Лист1'
+MAJOR_DIMENSION = 'ROWS'
+INPUT_OPTION = 'USER_ENTERED'
 ROW_COUNT = 100
 COLUMN_COUNT = 11
-SHEET_TYPE = 'GRID'
-SHEET_ID = 0
-MAJOR_DIMENSION = 'ROWS'
-RANGE = 'A1:E30'
-INPUT_OPTION = 'USER_ENTERED'
 TABLE_HEADER = [
     ['Отчет от', ''],
     ['Топ проектов по скорости закрытия'],
     ['Название проекта', 'Время сбора', 'Описание'],
 ]
+SPREADSHEET_BODY = dict(
+    properties=dict(title='', locale='ru_RU'),
+    sheets=[dict(properties=dict(
+        sheetType='GRID',
+        sheetId=0,
+        title='Лист1',
+        gridProperties=dict(rowCount=ROW_COUNT, columnCount=COLUMN_COUNT),
+    ))],
+)
 
 
-async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
+async def spreadsheets_create(wrapper_services: Aiogoogle) -> Tuple[str, str]:
     """Создаёт таблицу"""
     service = await wrapper_services.discover('sheets', 'v4')
-    spreadsheet_body = {
-        'properties': {
-            'title': SPREADSHEET_TITLE.format(
-                now_date_time=datetime.now().strftime(FORMAT)
-            ),
-            'locale': LOCALE,
-        },
-        'sheets': [
-            {
-                'properties': {
-                    'sheetType': SHEET_TYPE,
-                    'sheetId': SHEET_ID,
-                    'title': SHEET_TITLE,
-                    'gridProperties': {
-                        'rowCount': ROW_COUNT,
-                        'columnCount': COLUMN_COUNT,
-                    },
-                }
-            }
-        ],
-    }
+    spreadsheet_body = copy.deepcopy(SPREADSHEET_BODY)
+    spreadsheet_body['properties']['title'] = SPREADSHEET_TITLE.format(
+        now_date_time=datetime.now().strftime(FORMAT)
+    )
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
     )
-    return response['spreadsheetId']
+    return response['spreadsheetId'], response['spreadsheetUrl']
 
 
 async def set_user_permissions(
@@ -73,24 +62,31 @@ async def spreadsheets_update_value(
     spreadsheet_id: str, charity_projects: list, wrapper_services: Aiogoogle
 ) -> None:
     """Записывает данные в таблицу"""
-    now_date_time = datetime.now().strftime(FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-    table_values = TABLE_HEADER[:]
-    table_values[0][1] = now_date_time
-    table_values.extend(
-        [
-            [
-                str(project['name']),
-                str(project['duration']),
-                str(project['description']),
-            ]
-            for project in charity_projects
-        ]
-    )
+    table_header = copy.deepcopy(TABLE_HEADER)
+    table_header[0][1] = datetime.now().strftime(FORMAT)
+    table_values = [
+        *table_header,
+        *[list(map(str, project.values())) for project in charity_projects],
+    ]
+    row_count = len(table_values)
+    column_count = len(max(table_values, key=len))
+    if row_count > ROW_COUNT:
+        raise ValueError(
+            'Количество строк превышает допустимое значение: '
+            f'Максимально: {ROW_COUNT}, передано: {row_count}'
+        )
+    if column_count > COLUMN_COUNT:
+        raise ValueError(
+            'Количество столбцов превышает допустимое значение:'
+            f'Максимально: {COLUMN_COUNT}, передано: {column_count}'
+        )
     await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
             spreadsheetId=spreadsheet_id,
-            range=RANGE,
+            range=(
+                f'R1C1:R{row_count}C{column_count}'
+            ),
             valueInputOption=INPUT_OPTION,
             json={'majorDimension': MAJOR_DIMENSION, 'values': table_values},
         )
